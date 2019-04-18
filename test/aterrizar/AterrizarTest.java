fd4865c2ebe734678b35418b195438e9a040c12c
juan <pardojuanmanuel99@gmail.com>
2019-04-18 05:50:31 -0800

    Comienzo proyecto

---
+++ test/aterrizar/AterrizarTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aterrizar;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author juanm
+ */
+public class AterrizarTest {
+    
+    public AterrizarTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of main method, of class Aterrizar.
+     */
+    @org.junit.Test
+    public void testMain() {
+    }
+    
+}

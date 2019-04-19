dca67fce94a103491e0ff278ede8b91bfb6b6c7f
juan <pardojuanmanuel99@gmail.com>
2019-04-19 06:33:21 -0800

    Se agrego origen y destino a la clase Vuelo con sus correspondientes getters ya que no estaba siendo contemplado y es importante saber desde donde hasta donde es que va el vuelo

--- src/aerolinea/Vuelo.java
+++ src/aerolinea/Vuelo.java
@@ -5,14 +5,20 @@ import java.util.LinkedList;
 
 public class Vuelo {
     private LinkedList<AsientoVuelo> asientosVuelo;
+    private String origen;
+    private String destino;
 
-    public Vuelo() {
+    public Vuelo(String origen,String destino) {
         asientosVuelo = new LinkedList<>();
+        this.origen = origen;
+        this.destino = destino;
     }
 
-    public Vuelo(LinkedList<AsientoVuelo> asientosVuelo) {
+    public Vuelo(LinkedList<AsientoVuelo> asientosVuelo,String origen, String destino) {
         this.asientosVuelo = new LinkedList<>();
         this.asientosVuelo.addAll(asientosVuelo);
+        this.origen = origen;
+        this.destino = destino;
     }
     
     public void agregarAsiento(AsientoVuelo asientoVuelo){

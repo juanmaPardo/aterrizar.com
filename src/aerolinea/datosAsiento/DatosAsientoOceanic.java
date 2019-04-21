f6a7ff7825604d87240eab55b612373e4b4aada5
Nahuel Desimone <nahuel.desimone98@gmail.com>
2019-04-21 18:33:08 -0300

    Agrego la clase datos asiento Oceanic con los getters para todos sus atributos

--- src/aerolinea/datosAsiento/DatosAsientoOceanic.java
+++ src/aerolinea/datosAsiento/DatosAsientoOceanic.java
@@ -9,10 +9,37 @@ import fecha.FechaFormatoLatinoamericano;
 public class DatosAsientoOceanic extends DatosAsientoGeneral{
     private String horaDeSalida;
     private FechaFormatoLatinoamericano fechaDeSalida;
+    private Integer numeroDeAsiento;
+    private String codigoDeVuelo;
     
-    public DatosAsientoOceanic (DatosAsientoLanchita datosAsiento, CiudadPartida origen, CiudadDestino destino,String horaDeSalida, FechaFormatoLatinoamericano fechaDeSalida){
-        super(datosAsiento,origen,destino);
+    public DatosAsientoOceanic (PrecioAsiento precio, ClaseAsientoVuelo clase, UbicacionAsientoVuelo ubicacionAsiento, EstadoAsientoVuelo estado, String horaDeSalida,FechaFormatoLatinoamericano fechaDeSalida, Integer numeroDeAsiento, String codigoDeVuelo ){
+        super(precio, clase,ubicacionAsiento,estado);
         this.horaDeSalida = horaDeSalida;
         this.fechaDeSalida = fechaDeSalida;  
+        this.numeroDeAsiento = numeroDeAsiento;
+        this.codigoDeVuelo = codigoDeVuelo;
     }
+    
+    public String getHoraDeSalida(){
+        return horaDeSalida;
+    }
+    
+    public String getFechaDeSalida(){
+        return fechaDeSalida.representacionEnLatinoamericano();
+    }
+    
+    public Integer getNumeroDeAsiento(){
+        return numeroDeAsiento;
+    }
+    
+    public String getCodigoDeVuelo(){
+        return codigoDeVuelo;  
+}
+
+    
+    
+    
+    
+    
+    
 }
